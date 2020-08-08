8c4bc44942a1f6ee60e6f0ddf982191fef7d3fb9
Aranganathan Rathinavelu <arangates@gmail.com>
2020-08-08 12:17:08 +0530

    fix overflow

--- components/Analytics/Chart.tsx
+++ components/Analytics/Chart.tsx
@@ -1,4 +1,11 @@
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 interface chartProps {
   items: object[];
@@ -8,35 +15,37 @@ interface chartProps {
 
 export const Chart = ({ items, title, colour }: chartProps) => {
   return (
-    <div className='graph-card--shadow flex-auto px-6 py-5 bg-white dark:bg-gray-600 shadow-md rounded-lg cursor-pointer mb-8 md:mb-0'>
-      <h2 className='text-2xl h-10 text-gray-700'>{title}</h2>
-      <BarChart
-        width={500}
-        height={400}
-        data={items}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis stroke={''} dataKey='name' tick={false} />
-        <YAxis stroke={''} tick={{ fill: '#B0BEC5' }} />
-        <Tooltip
-          contentStyle={{
-            borderRadius: '20px',
-            borderColor: colour,
+    <div className='graph-card--shadow flex-auto py-8 bg-white dark:bg-gray-600 shadow-md rounded-lg cursor-pointer mb-8 md:mb-0'>
+      <h2 className='text-2xl h-10 text-gray-700 px-8'>{title}</h2>
+      <ResponsiveContainer width='100%' height='100%'>
+        <BarChart
+          width={500}
+          height={400}
+          data={items}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
           }}
-          formatter={(name) => [`${title}: ${name}`]}
-        />
-        <Bar
-          dataKey='val'
-          fill={colour}
-          barSize={15}
-          radius={[10, 10, 10, 10]}
-        />
-      </BarChart>
+        >
+          <XAxis stroke={''} dataKey='name' tick={false} />
+          <YAxis stroke={''} tick={{ fill: '#B0BEC5' }} />
+          <Tooltip
+            contentStyle={{
+              borderRadius: '20px',
+              borderColor: colour,
+            }}
+            formatter={(name) => [`${title}: ${name}`]}
+          />
+          <Bar
+            dataKey='val'
+            fill={colour}
+            barSize={10}
+            radius={[10, 10, 10, 10]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
--- containers/Main.tsx
+++ containers/Main.tsx
@@ -1,6 +1,6 @@
 export const MainContainer = ({ children }: any) => {
   return (
-    <main className='py-1 xl:flex-1 xl:overflow-x-hidden px-5'>
+    <main className='py-1 xl:flex-1 xl:overflow-auto px-5'>
       {children}
     </main>
   );
